2d9cbd3cf7c10713532b378525e986116f1b8aaa
Jennifer Jang <jennjang33@gmail.com>
2021-10-12 14:02:04 -0400

    oct 12 downloaded useDraggableScroll; used it in Image Overflow component

--- src/components/works/projects/sections/section-components/ImageOverflow.js
+++ src/components/works/projects/sections/section-components/ImageOverflow.js
@@ -1,15 +1,35 @@
-import ImageFadeIn from '../../../../../customHooks/imageFadeIn'
-import ScrollHorizontal from './mouseScroll.js'
+import ImageFadeIn from '../../../../../customHooks/imageFadeIn';
+import  useDraggableScroll from 'use-draggable-scroll';
+import {useRef, useState,useEffect} from 'react'
+
 const ImageOverflow = ({m, images=[], height="50vh", style, titles}) => {
 
+  const ref = useRef(null);
+  const [whileMouseDown, setWhileMouseDown] = useState(false)
+
+  const { onMouseDown } = useDraggableScroll(ref);
+
+
+
+
+
   return (
-    <div className="Row" style={{position:"relative", zIndex:"25", left:"0px", height:height ,overflowX:"scroll", width:m? "100%":"100vw",padding:"4rem"}}>
+    <div className="Row" ref={ref} onMouseDown={onMouseDown} style={{
+      position:"relative",
+      zIndex:"25",
+      left:"0px",
+      height:height,
+      overflowX:"scroll",
+      width:m? "100%":"100vw",
+      padding:"4rem"}}>
+
      {images && images.map((image, number)=>{
        return <>
        <div className="overline" style={{alignSelf:"flex-start", justifySelf:"flex-start", paddingTop:"1rem", padding:"0 2rem"}}>
          {titles && titles[number]}
        </div>
-       <ImageFadeIn src={image} style={{
+       <ImageFadeIn src={image} draggable="false" style={{
+         pointerEvents:"none",
          zIndex:"25",
          position:"absolute",
          width: "fit-content",
