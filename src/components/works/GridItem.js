5b35279f4eb58a46245f9c6ba8049c87296e0b03
Jennifer Jang <85824661+JCJang@users.noreply.github.com>
2021-12-24 16:13:12 -0500

    dec 24 update

--- src/components/works/GridItem.js
+++ src/components/works/GridItem.js
@@ -9,15 +9,15 @@ const GridItem = ({m, l, direction, routeName, image, role, title, subtitle, spe
   const [buttonHover, setButtonHover] = useState(false)
 
 const gradientSetting = () =>{
-  if(cardHover) return `transition grid grid-lr-hover ${classes}`;
-  return `transition grid grid-lr ${classes}` ;
+  if(cardHover) return `transition grid-lr-hover`;
+  return `transition grid-lr` ;
 }
 
 
   return (
 
     
-    <div className="transition" onFocus={() => setCardHover(true)}
+    <div className="Column transition" onFocus={() => setCardHover(true)}
       onBlur={() => setCardHover(false)}
       onMouseEnter={() => setCardHover(true)}
       onMouseLeave={() => setCardHover(false)}
@@ -25,50 +25,65 @@ const gradientSetting = () =>{
       onMouseUp={() => setCardHover(false)}  
       style = {{
       borderRadius: cardHover?"3px":"8px",
-      borderBottom: cardHover? "6px solid var(--lilac)":"6px solid var(--cinerous)",
+      borderBottom: buttonHover? "6px solid var(--velvet)":cardHover?"6px solid var(--desert-rose)":"6px solid var(--cinerous)",
       boxShadow:cardHover?"none":"rgba(60, 84, 82, 0.25) 0px 6px 12px -2px, rgba(60, 84, 82, 0.3) 0px 3px 7px -3px",
       width:"35vw",
       margin:l?"0 1.5rem":"1rem 0",
       background:buttonHover?"none":"var(--lilac)",
-      outline:buttonHover?"1.5px solid var(--midnight)":"var(--lilac)"
+      outline:buttonHover?"1.5px solid var(--midnight)":"var(--lilac)",
+      transition: 'border-bottom 0s'
   }}>
 
-    <div className={gradientSetting()} style={{
+    <div className="transition" style={{
       width:"35vw",
-      transform:cardHover && "translateY(-25vh)",
-    }}>
+      height:"50%",
+      position:"relative",
+      borderRadius:buttonHover || cardHover?"3px":"8px",
+      overflow:"hidden"
+      }}>
+      <div className={gradientSetting()} style={{width:"100%", height:"100%", position:"absolute",transform:buttonHover? "": cardHover? "translateY(-25vh)":"",
+}}>
           <ImageFadeIn role="presentation" className="gridImage transition" src={image} style={{ filter:cardHover?"saturate(0.6)":"saturate(0.1) opacity(0.8)",
           }}/>
 <div className="transition Column" style = {{
-    height: "50%",
+    height: "100%",
     width: "100%",
     position: "relative",
     zIndex: "10",
     color: "var(--lilac)",
     lineHeight: "5rem",
     justifyContent: "center",
+    alignContent:"center",
     padding:"1rem 4rem 0 1rem",
   }}
    >
     <Fade delay={700}>
-          <div style={{fontWeight:"400",font:m?"2rem/1.3 'Lusitana',sans-serif":"1.5rem/1.2 'Lusitana',sans-serif", letterSpacing:"0.0156rem",textTransform:"", margin:"0.6rem 0"}}>{title}</div>
+          <div style={{fontWeight:"400",font:m?"2.5rem/1.3 'Lusitana',sans-serif":"1.5rem/1.2 'Lusitana',sans-serif", letterSpacing:"0.0156rem",textTransform:"", margin:"0.6rem 0"}}>{title}</div>
     </Fade>
     <Fade bottom delay={900}>
 
-        <div className="subtitle1" style={{lineHeight:"1.7rem", fontWeight:"400",fontSize:"1.2rem", marginBottom:"0.2rem"}}>{subtitle}</div>
+        <div className="subtitle1" style={{lineHeight:"2.3rem", fontWeight:"400",fontSize:"1.5rem", marginBottom:"0.2rem"}}>{subtitle}</div>
     </Fade>
 
     </div>
+
     </div>
-    <div className="Column">
+    <div className="Column-center">
+      <div>carousel here asd;foasjd;fiajsd;lfajsd;lfjasldfjasldkfja;sldjfalsdikjfla;sidkjf;laisdjf;laiskdjf;laksdjf;lasikdjfsdl</div>
+      </div>
+
+    </div>
+    <div className="Column" style={{height:"calc(50% - 3rem)",  padding:"1.5rem"}}>
     <Fade top delay={500}>
-        <div style={{font:"0.8rem/1.5 'Yaldevi'", marginTop:"0.5rem"}}>{role}</div>
+        <div style={{font:"1.2rem/1.5 'Yaldevi'", marginTop:"0.5rem",color:"var(--works-text)"}}>{role}</div>
     </Fade>
 
-    <div>
-        hihihihihihi
+    <div className='body2' style={{color:"var(--midnight"}}>
+        {introduction}
     </div>
 
+    </div>
+    
     <Link to={routeName}
      onFocus={() => setButtonHover(true)}
      onBlur={() => setButtonHover(false)}
@@ -79,14 +94,15 @@ const gradientSetting = () =>{
       style={{
         justifySelf:"flex-end",
         width:"100%",
+        height:"3rem",
         color:"var(--contact-text)",
         padding:"1rem",
+        paddingBottom: "calc( 1rem - 6px)",
         textAlign:"right",
         textDecoration: "none",
         opacity:cardHover? "1" : "0",
-      background:buttonHover?"var(--velvet)":"var(--desert-rose)"
+        background:buttonHover?"var(--velvet)":"var(--desert-rose)"
     }}>GO TO CASE STUDY</Link>
-    </div>
     </div>
 
   )
